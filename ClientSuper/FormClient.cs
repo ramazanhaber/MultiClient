9d67095731820e01b644e31fbafd0c3cd9eed2ef
Ramazan HABER <ramazanhaberr@gmail.com>
2022-09-29 12:33:57 +0300

    Add project files.

---
+++ ClientSuper/FormClient.cs
@@ -0,0 +1,44 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClientSuper
+{
+    public partial class FormClient : Form
+    {
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpClient client = new SimpleTcpClient("127.0.0.1:9000");
+        private void FormClient_Load(object sender, EventArgs e)
+        {
+            client.Events.Connected += Connected;
+            client.Events.Disconnected += Disconnected;
+            client.Events.DataReceived += DataReceived;
+        }
+
+         void Connected(object sender, ConnectionEventArgs e)
+        {
+            Console.WriteLine($"*** Server {e.IpPort} connected");
+        }
+
+         void Disconnected(object sender, ConnectionEventArgs e)
+        {
+            Console.WriteLine($"*** Server {e.IpPort} disconnected");
+        }
+
+         void DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine($"[{e.IpPort}] {Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count)}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Connect();
+            client.Send(textBox1.Text);
+        }
+    }
+}
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,46 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server = new SimpleTcpServer("127.0.0.1:9000");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server.Events.ClientConnected += ClientConnected;
+            server.Events.ClientDisconnected += ClientDisconnected;
+            server.Events.DataReceived += DataReceived;
+            server.Start();
+            server.Send("[ClientIp:Port]", "Hello, world!");
+        }
+
+        void ClientConnected(object sender, ConnectionEventArgs e)
+        {
+            Console.WriteLine($"[{e.IpPort}] client connected");
+        }
+
+        void ClientDisconnected(object sender, ConnectionEventArgs e)
+        {
+            Console.WriteLine($"[{e.IpPort}] client disconnected: {e.Reason}");
+        }
+
+        void DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string mesaj = $"[{e.IpPort}]: {Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count)}";
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox1.Text = textBox1.Text + "\n" + mesaj;
+            }));
+        }
+
+    }
+}
